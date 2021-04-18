34977de31a0a4a9a339659ab92b3c3634744e088
ruaryvina06 <ruaryvina06@gmail.com>
2021-04-18 22:10:29 +0700

    sum

---
+++ Tugas Minggu_5/Graph/GraphApp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
+
+/**
+ *
+ * @ajengtiaswulandari
+ */
+public class GraphApp {
+    public static void main(String[] args) {
+        
+        Graph theGraph = new Graph();
+        
+        theGraph.addvertex('A'); //0
+        theGraph.addvertex('B'); //1
+        theGraph.addvertex('C'); //2
+        theGraph.addvertex('D'); //3
+        theGraph.addvertex('E'); //4
+        
+        theGraph.addEdge(0, 1); //AB
+        theGraph.addEdge(1, 2); //BC
+        theGraph.addEdge(2, 3); //AD
+        theGraph.addEdge(3, 4); //DE
+        
+        //theGraph.bfs
+        
+        theGraph.bfs();
+//        theGraph.display();
+        
+    }
+    
+}
